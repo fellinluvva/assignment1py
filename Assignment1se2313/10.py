cead192980dc322a2b3c4c6718d72b79fbac7c36
Friday <230040@astanait.edu.kz>
2023-12-23 23:19:04 +0600

    forgot to delete code nvm

--- Assignment1se2313/10.py
+++ Assignment1se2313/10.py
@@ -2,6 +2,8 @@ name = input("Enter your name: ")
 print(f"Welcome, {name}!")  # anything instead of 'Welcome' to greet user
 
 '''
+# don't pay attention to this i forgot to delete it when
+# i send code to git so i decided to leave it here
 name = "Carl"
 print(f"-You picked the wrong house fool!")
 print(f"-Hey, aye-ye-ye, Big Smoke! It's me, {name}! Chill, chill!")
